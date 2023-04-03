f4ec65a24c77c033f12246c7b9ebaf1474895676
unknown <dyd1308@naver.com>
2023-04-04 01:06:26 +0900

    어렵네

--- python/실습및과제/2230403 5247 연산.py
+++ python/실습및과제/2230403 5247 연산.py
@@ -10,16 +10,47 @@
 # [출력]
 # 각 줄마다 "#T" (T는 테스트 케이스 번호)를 출력한 뒤, 답을 출력한다.
 
-# import sys
-# sys.stdin = open('2230403 5247 연산.txt')
+import sys
+sys.stdin = open('2230403 5247 연산.txt')
 
 # [문제풀이]
 # 0. +1, -1, *2, -10 의 네 가지 연산을 이용해서  최소 몇 번의 연산을 거치면 N이 M이 되는지를 확인하는 문제이다.
 # 1. DFS로 풀어야지 생각했지만 BFS로 풀어야, 무한반복을 멈출수 있다.(멈춰!!!!)
 # 2. 중간 결과도 항상 백만 이하의 자연수 이어야 하므로 0 < x < 1000000의 조건을 달고 있어야한다.
 
+def find(n, count):
+    queue = [(n, count)]
+    visited = set()
+    visited.add(n)
+
+    while queue:
+        n, count = queue.pop(0)
+
+        if n == M:
+            return count
+        
+        if n > 1000000:
+            continue
+        
+        if n+1 not in visited:
+            queue.append((n+1, count+1))
+            visited.add(n+1)
+
+        if n-1 not in visited:
+            queue.append((n-1, count+1))
+            visited.add(n-1)
+
+        if n*2 not in visited:
+            queue.append((n*2, count+1))
+            visited.add(n*2)
+
+        if n-10 not in visited:
+            queue.append((n-10, count+1))
+            visited.add(n-10)
+
 
 T = int(input())
 for testcase in range(1, T+1):
     N, M = map(int, input().split())
-    
+    result = find(N, 0)
+    print(f'#{testcase} {result}')
