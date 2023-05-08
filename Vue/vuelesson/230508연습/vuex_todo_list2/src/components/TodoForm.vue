acddb74edbd037ac9dd4e358c9d67a6da441b3b3
unknown <dyd1308@naver.com>
2023-05-08 22:39:18 +0900

    연습함

---
+++ Vue/vuelesson/230508연습/vuex_todo_list2/src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model.trim="todoInput">
+    <button @click="onAddBtnClick"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'TodoForm',
+  data(){
+    return{
+      todoInput: ""
+    }
+  },
+  methods:{
+    onAddBtnClick(){
+      if(this.todoInput){
+        this.$store.dispatch('addTodo', this.todoInput),
+        this.todoInput = ""
+      }else{
+        alert('내용을 입력하세요')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vuelesson/230508연습/vuex_todo_list2/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+    <TodoListItem v-for="todo in todoList" :key="todo.date" :todo="todo"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'TodoList',
+  components:{
+    TodoListItem,
+  },
+  computed:{
+    todoList(){
+      return this.$store.state.todoList
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
